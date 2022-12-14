f018ba3846500a73c21cafe0e062574aead038f2
smills17 <smills17@gmail.com>
2022-12-13 23:53:47 -0700

    added reacts, fixed db issues, added async counter

--- discord-to-sql.py
+++ discord-to-sql.py
@@ -2,18 +2,22 @@ import discord
 import os
 from dotenv import load_dotenv
 import sqlite3
+import sys
+import asyncio
 
 class DiscordClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # an attribute we can access from our task
-        self.all_messages = []
+        self.message_counter = 0
         self.dbconn = sqlite3.connect(DB_FILENAME)
         self.cursor = self.dbconn.cursor()
         self.cursor.executescript(sql_template_script)
         self.dbconn.commit()
 
+    async def setup_hook(self) -> None:
+        self.status_checker = self.loop.create_task(self.check_status())
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -27,9 +31,19 @@ class DiscordClient(discord.Client):
             INSERT OR IGNORE INTO user VALUES (?,?)
             """,(message.author.id,message.author.name))
 
-        print("all messages scanned")
-        self.dbconn.commit()
-        print("db saved")
+            for react in message.reactions: 
+                self.cursor.execute("""
+                INSERT OR IGNORE INTO react VALUES (?,?,?)
+                """,(message.id, str(react.emoji),react.count))
+            self.dbconn.commit()
+            self.message_counter += 1
+        print("scrape complete")
+
+    async def check_status(self):
+        while not self.is_closed():
+            print(self.message_counter)
+            await asyncio.sleep(10)
+
 
 load_dotenv()
 BOT_API_KEY = os.getenv('BOT_API_KEY')
