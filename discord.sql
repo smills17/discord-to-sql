5e7e4ff3c55182348d7bf2646c606045fd9cbae9
smills17 <smills17@gmail.com>
2022-12-14 00:17:01 -0700

    fixed message keys

--- discord.sql
+++ discord.sql
@@ -1,3 +1,3 @@
 CREATE TABLE IF NOT EXISTS user(id,name, UNIQUE(id));
-CREATE TABLE IF NOT EXISTS message(id,user_id,content);
+CREATE TABLE IF NOT EXISTS message(id,user_id,content, UNIQUE(id));
 CREATE TABLE IF NOT EXISTS react(message_id,emoji_name,quantity, UNIQUE(message_id, emoji_name));
